3c21bc75ae761c4009aff8651553ee9d70b6999f
tdude92 <trevorguanhao@gmail.com>
2019-12-15 03:23:15 -0500

    add functionality to blockchain

--- block.py
+++ block.py
@@ -3,7 +3,6 @@ import pickle as pkl
 import datetime
 import binascii
 
-
 class Block():
     def __init__(self, index, timestamp, data, previous_hash):
         self.index = index
@@ -21,6 +20,7 @@ class Block():
         key.update(str(self.previous_hash).encode('utf-8'))
         return key.hexdigest()
 
+
 class Chain():
     def __init__(self):
         with open('chain.pkl', 'rb') as r:
@@ -31,8 +31,10 @@ class Chain():
                 with open('chain.pkl', 'wb') as w:
                     pkl.dump(self.blocks, w)
 
+
     def create_gen_block(self):
         return Block(0, datetime.datetime.utcnow(), 'BlockCondom', 'arbitrary')
+
 
     def add_block(self, d):
         self.blocks.append( Block(
@@ -42,35 +44,57 @@ class Chain():
                             self.blocks[-1].hash) )
         with open('chain.pkl', 'wb') as w:                    
             pkl.dump(self.blocks, w)
-        print(self.blocks)
+
+    
+    def prune(self, idx):
+        # Chop off all blocks after a fraudulent block.
+        self.blocks = self.blocks[:idx]
+        with open("chain.pkl", "wb") as w:
+            pkl.dump(self.blocks, w)
+
 
     def chain_length(self):
-        return self.block - 1 # exclude genesis block
+        return len(self.blocks) - 1 # exclude genesis block
+
 
     def verify(self, verbose=True): 
         flag = True
+        idx = None
         for i in range(1,len(self.blocks)):
             if self.blocks[i].index != i:
                 flag = False
+                idx = i
                 if verbose:
                     print(f'Wrong block index at block {i}.')
             if self.blocks[i-1].hash != self.blocks[i].previous_hash:
                 flag = False
+                idx = i
                 if verbose:
                     print(f'Wrong previous hash at block {i}.')
             if self.blocks[i].hash != self.blocks[i].hashing():
                 flag = False
+                idx = i
                 if verbose:
                     print(f'Wrong hash at block {i}.')
             if self.blocks[i-1].timestamp >= self.blocks[i].timestamp:
                 flag = False
+                idx = i
                 if verbose:
                     print(f'Backdating at block {i}.')
-        return flag
+        return flag, idx
+
 
     def search(self, userid):
         for block in self.blocks:
             temp = []
-            if block.data.sender-id == userid or block.data.recipient-id == userid:
+            if block.data["sender-email"] == userid or block.data["recipient-email"] == userid:
                 temp.append(block)
         return temp
+    
+
+    def get_last_transaction(self, userid):
+        for i in range(len(self.blocks) - 1, -1, -1):
+            if self.blocks[i].data["sender-email"] == user_email:
+                return self.blocks[i]
+        else:
+            return None

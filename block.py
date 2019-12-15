02e1b5b51f69b2a588c2903f4d7303d5f33eefae
tdude92 <trevorguanhao@gmail.com>
2019-12-15 03:21:54 -0500

    push

---
+++ block.py
@@ -0,0 +1,100 @@
+import hashlib as hl
+import pickle as pkl
+import datetime
+import binascii
+
+class Block():
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hashing()
+
+
+    def hashing(self):
+        key = hl.sha256()
+        key.update(str(self.index).encode('utf-8'))
+        key.update(str(self.timestamp).encode('utf-8'))
+        key.update(str(self.data).encode('utf-8'))
+        key.update(str(self.previous_hash).encode('utf-8'))
+        return key.hexdigest()
+
+
+class Chain():
+    def __init__(self):
+        with open('chain.pkl', 'rb') as r:
+            try:
+                self.blocks = pkl.load(r)
+            except EOFError:
+                self.blocks = [self.create_gen_block()]
+                with open('chain.pkl', 'wb') as w:
+                    pkl.dump(self.blocks, w)
+
+
+    def create_gen_block(self):
+        return Block(0, datetime.datetime.utcnow(), 'BlockCondom', 'arbitrary')
+
+
+    def add_block(self, d):
+        self.blocks.append( Block(
+                            len(self.blocks), 
+                            datetime.datetime.utcnow(), 
+                            d, 
+                            self.blocks[-1].hash) )
+        with open('chain.pkl', 'wb') as w:                    
+            pkl.dump(self.blocks, w)
+
+    
+    def prune(self, idx):
+        # Chop off all blocks after a fraudulent block.
+        self.blocks = self.blocks[:idx]
+        with open("chain.pkl", "wb") as w:
+            pkl.dump(self.blocks, w)
+
+
+    def chain_length(self):
+        return len(self.blocks) - 1 # exclude genesis block
+
+
+    def verify(self, verbose=True): 
+        flag = True
+        idx = None
+        for i in range(1,len(self.blocks)):
+            if self.blocks[i].index != i:
+                flag = False
+                idx = i
+                if verbose:
+                    print(f'Wrong block index at block {i}.')
+            if self.blocks[i-1].hash != self.blocks[i].previous_hash:
+                flag = False
+                idx = i
+                if verbose:
+                    print(f'Wrong previous hash at block {i}.')
+            if self.blocks[i].hash != self.blocks[i].hashing():
+                flag = False
+                idx = i
+                if verbose:
+                    print(f'Wrong hash at block {i}.')
+            if self.blocks[i-1].timestamp >= self.blocks[i].timestamp:
+                flag = False
+                idx = i
+                if verbose:
+                    print(f'Backdating at block {i}.')
+        return flag, idx
+
+
+    def search(self, userid):
+        for block in self.blocks:
+            temp = []
+            if block.data["sender-email"] == userid or block.data["recipient-email"] == userid:
+                temp.append(block)
+        return temp
+    
+
+    def get_last_transaction(self, userid):
+        for i in range(len(self.blocks) - 1, -1, -1):
+            if self.blocks[i].data["sender-email"] == user_email:
+                return self.blocks[i]
+        else:
+            return None
--- server.py
+++ server.py
@@ -1,10 +1,42 @@
 import flask
+import torch
+import torch.nn as nn
 import shodan
+import vincenty
+import pickle
+import block
+
+# Exception class for server errors
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status = None, payload = None):
+        self.message = message
+        if status_code != None:
+            status_code = status
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+
+
+# Get Model and Blockchain (ANONYMOUS FUNCTION IN classifier IS PLACEHOLDER)
+block_chain = block.Chain()
+classifier = lambda x: x
 
 # APIs
 shodan_api = shodan.Shodan("bPeoBUXYuSxvw4YlP0j47yBzsPsaNiYt")
 
+# Flask server
 server = flask.Flask(__name__)
+
+@server.errorhandler(InvalidUsage)
+def handle_error(error):
+    response = flask.jsonify()
+    response.status_code = error.status_code
+    return response
 
 
 @server.route("/")
@@ -13,21 +45,12 @@ def home_page():
     return "Hello, World!"
 
 
-@server.route("/check_proxy", methods = ["GET"])
-def check_proxy():
-    ip = flask.request.remote_addr
-    print("IP:", ip)
-    ip_info = shodan_api.host(ip)
-    if "tags" in ip_info and "vpn" in ip_info["tags"] and "cloud" in ip_info["tags"]:
-        return "ur on a proxy m8"
-    else:
-        return "ur not o a proxy"
-
-
-@server.route("/check_fraud", methods = ["GET", "POST"])
-def check_fraud():
+@server.route("/verify", methods = ["GET", "POST"])
+def verify():
     # Request body:
+    #   "amt"
     #   "location"
+    #   "avg-location"
     #   "time"
     #   "device-ip"
     #   "os-id"
@@ -36,11 +59,40 @@ def check_fraud():
     #   "device-model"
     #   "sender-email"
     #   "recipient-email"
-    #   "sender-id"
-    #   "recipient-id"
-    data = flask.request.json
 
-    return data
+    curr_transaction = flask.request.json
+    last_transaction = block_chain.get_last_transaction(curr_transaction["sender-email"])
+
+    # Check if IP is from a VPN (on cloud)
+    ip_info = shodan_api.host(curr_transaction["device-ip"])
+    on_proxy = 0
+    if "tags" in ip_info and "cloud" in ip_info["tags"]:
+        on_proxy = 1
+    
+    # Normalized to $100
+    amount = curr_transaction["amt"] / 100
+
+    # Normalized (from milliseconds) to days
+    time_diff = (curr_transaction["time"] - prev_transaction["time"]) / 31536000000
+
+    # Normalized to 1/2 of the Earth's circumference (in kilometres)
+    distance = vincenty.vincenty(curr_transaction["location"], curr_transaction["avg-location"]) / 20000
+
+    model_input = []
+    output = classifier(model_input)
+
+    if output[1] > 0.5:
+        return flask.jsonify(genuine = False)
+    elif output[0] > 0.5:
+        # Verify block chain and delete fraudulent blocks.
+        verified, fraudulent_idx = block_chain.verify()
+        if not verified:
+            block_chain.prune(fraudulent_idx)
+
+        block_chain.add_block(curr_transaction)
+        return flask.jsonify(genuine = True)
+    else:
+        raise InvalidUsage("Internal Server Error", 500)
 
 
-server.run(host = "0.0.0.0", port = "42069")
+server.run(host = "0.0.0.0", port = "5050")
